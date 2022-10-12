e95cb85ccfbe18996d0818d42a0810eb29b3230b
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-10-12 18:54:59 -0500

    41 Backend Hello World API (#6)
    * setting up the CORS
    
    * finished setting up CORS
    
    * finishing Hello World
    
    * PR comments

---
+++ src/main/java/com/my/FoodTruckBackend/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.my.FoodTruckBackend;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorld {
+    private String message;
+}
