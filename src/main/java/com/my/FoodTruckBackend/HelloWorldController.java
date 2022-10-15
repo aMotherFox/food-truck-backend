03398c7efb61c8915c17535dee4968ab575f293d
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-10-14 20:40:00 -0500

    Adding prettier

--- src/main/java/com/my/FoodTruckBackend/HelloWorldController.java
+++ src/main/java/com/my/FoodTruckBackend/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
     @GetMapping("/helloWorld")
     public HelloWorld helloWorld() {
         return new HelloWorld("HELLO WORLD");
