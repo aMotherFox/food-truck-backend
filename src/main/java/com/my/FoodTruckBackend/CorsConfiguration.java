4889b79920b33633b9f43e59530e4833f2b8d81e
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-10-17 15:31:02 -0500

    Adding prettier (#7)
    * Adding prettier
    
    * Adding prettier

--- src/main/java/com/my/FoodTruckBackend/CorsConfiguration.java
+++ src/main/java/com/my/FoodTruckBackend/CorsConfiguration.java
@@ -13,10 +13,11 @@ public class CorsConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins("http://localhost:3000");
             }
         };
-    };
+    }
 }
