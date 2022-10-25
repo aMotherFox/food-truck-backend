0a3bb3471cdaddc05594813aa4711d2e39cf79fc
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-10-25 16:34:22 -0500

    adding valid annotations

--- src/main/java/com/my/FoodTruckBackend/customer/NewCustomerRequestBody.java
+++ src/main/java/com/my/FoodTruckBackend/customer/NewCustomerRequestBody.java
@@ -5,16 +5,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class NewCustomerRequestBody {
+    @NotBlank @NotNull @NotEmpty
     private String firstName;
+
+    @NotBlank @NotNull @NotEmpty
     private String lastName;
 
     @Email
     private String email;
+
+    @NotBlank @NotNull @NotEmpty
     private String password;
+
+    @NotBlank @NotNull @NotEmpty
     private String confirmPassword;
 }
