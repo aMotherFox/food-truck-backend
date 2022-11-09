e7a0489952b8a9d6390a0962202305be0d894a1f
vp <victoria.portela03@gmail.com>
2022-11-08 18:57:57 -0600

    pseudo coding

--- src/main/java/com/my/FoodTruckBackend/customer/login/Login.java
+++ src/main/java/com/my/FoodTruckBackend/customer/login/Login.java
@@ -8,8 +8,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Login {
-    private Integer id;
     private String email;
     private String password;
 
 }
+
+//login endpoint
+/*
+* front end sends email and password to the back end
+* BE receives this data
+* checks the db for a matching email
+* checks the db for matching password
+* if both of these are found,
+* return a safe user with id, email, and password
+* give a 200 success code
+*
+* */
