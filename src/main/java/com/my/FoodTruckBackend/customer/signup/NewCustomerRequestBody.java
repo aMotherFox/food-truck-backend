a863f42260413342200f45d99b54595e6409a857
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-11-14 20:06:29 -0600

    62 Min/Max Error message bug (#10)

--- src/main/java/com/my/FoodTruckBackend/customer/signup/NewCustomerRequestBody.java
+++ src/main/java/com/my/FoodTruckBackend/customer/signup/NewCustomerRequestBody.java
@@ -12,11 +12,11 @@ import lombok.NoArgsConstructor;
 @Data
 public class NewCustomerRequestBody {
     @Size(min = 2, max = 50)
-    @NotBlank(message = "Please enter your first name")
+    @NotBlank(message = "Please enter your first name, between 2-50 characters")
     private String firstName;
 
     @Size(min = 2, max = 50)
-    @NotBlank(message = "Please enter your last name")
+    @NotBlank(message = "Please enter your last name, between 2-50 characters")
     private String lastName;
 
     @Email(message = "Invalid email, please enter valid email address")
@@ -24,10 +24,10 @@ public class NewCustomerRequestBody {
     private String email;
 
     @Size(min = 8, max = 50)
-    @NotBlank(message = "Please choose a unique password")
+    @NotBlank(message = "Please choose a unique password, between 8-50 characters")
     private String password;
 
     @Size(min = 8, max = 50)
-    @NotBlank(message = "Please re-enter your unique password")
+    @NotBlank(message = "Please re-enter your unique password, between 8-50 characters")
     private String confirmPassword;
 }
