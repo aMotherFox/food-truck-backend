4c4fe091209fab1c1bcd983c52f574e62cb905a0
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2022-11-21 19:15:56 -0600

    sending for PR

--- src/main/java/com/my/FoodTruckBackend/customer/signup/CustomerService.java
+++ src/main/java/com/my/FoodTruckBackend/customer/signup/CustomerService.java
@@ -21,17 +21,21 @@ public class CustomerService {
     }
 
     public CustomerDto createNewCustomers(NewCustomerRequestBody newCustomerRequestBody) {
-//        if (!newCustomerRequestBody.getPassword().equals(newCustomerRequestBody.getConfirmPassword())) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "passwords do not match");
-//        }
 
         if (newCustomerRequestBody.getPassword().equals(newCustomerRequestBody.getConfirmPassword())) {
             log.info("Passwords match");
         } else {
             log.error("Passwords do not match");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "passwords do not match");
+        } if (newCustomerRequestBody.getFirstName().length() < 2 || newCustomerRequestBody.getFirstName().length() > 50) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please enter your first name, between 2-50 characters");
+        } if (newCustomerRequestBody.getLastName().length() < 2 || newCustomerRequestBody.getLastName().length() > 50) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please enter your last name, between 2-50 characters");
+        } if (newCustomerRequestBody.getPassword().length() < 2 || newCustomerRequestBody.getPassword().length() > 50) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please enter a unique password, between 2-50 characters");
+        } if (newCustomerRequestBody.getConfirmPassword().length() < 2 || newCustomerRequestBody.getConfirmPassword().length() > 50) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Please re-enter your unique password, between 8-50 characters");
         }
-
         return customerRepository.createNewCustomers(newCustomerRequestBody);
     }
 }
