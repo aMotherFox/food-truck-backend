830fdee311e16286785f81da6f078deb0d858f14
aMotherFox <102123418+aMotherFox@users.noreply.github.com>
2023-01-04 19:22:35 -0600

    added logs

--- src/main/java/com/my/FoodTruckBackend/appetizer/AppetizerController.java
+++ src/main/java/com/my/FoodTruckBackend/appetizer/AppetizerController.java
@@ -2,6 +2,7 @@ package com.my.FoodTruckBackend.appetizer;
 
 import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,16 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class AppetizerController {
     private final AppetizerService appetizerService;
 
     @PostMapping("/appetizers")
     public Appetizer createNewAppetizers(@RequestBody NewAppetizerRequestBody newAppetizerRequestBody) {
+        log.info("appetizer created");
         return appetizerService.createNewAppetizers(newAppetizerRequestBody);
     }
 
-    @GetMapping("/appetizer_menu")
+    @GetMapping("/appetizers")
     public ArrayList<Appetizer> getListOfAppetizers() {
+        log.info("we are getting a list of appetizers");
         return appetizerService.getListOfAppetizers();
     }
 }
