6d4d882d053df9681000e39ccd5fa684dbbf958f
vp <victoria.portela03@gmail.com>
2023-01-25 21:22:13 -0500

    this was not planned very well

--- src/main/java/com/my/FoodTruckBackend/order/OrderService.java
+++ src/main/java/com/my/FoodTruckBackend/order/OrderService.java
@@ -1,10 +1,13 @@
 package com.my.FoodTruckBackend.order;
 
 import com.my.FoodTruckBackend.appetizer.AppetizerRepository;
+import com.my.FoodTruckBackend.entree.Entree;
 import com.my.FoodTruckBackend.entree.EntreeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class OrderService {
 
     public OrderDTO createOrder(NewOrderRequestBody newOrderRequestBody) {
         Order newOrder = orderRepository.createOrder(newOrderRequestBody);
-
+        // need to get entrees and apps by their ID, is it ok to do this in this story??
+        return new OrderDTO();
     }
 }
